969d8c6e0aebb5d2be7ad30167a2eba01e61407c
kamrote <85530913+kamrote@users.noreply.github.com>
2022-11-05 02:35:51 -0500

    initial

---
+++ README.md
@@ -0,0 +1,43 @@
+# Screenshare movies/youtube to discord
+
+## Notes
+- This is mainly made to work on linux, may work on windows also, but doubt it.
+- if your planning on streaming a 1080p movie, or even at 720. your gonna need an actual decent server, otherwise the screenshare is just gonna lag terribly
+- For anybody wondering why this uses RobotJS to click the screenshare instead of just making puppeteer auto set the screenshare tab, for whatever reason even after 5 years of people asking, puppeteer wont add a option to accept screensharing audio.
+- If you select 1920x1080 as a resolution but discord still says the quality is 720, it is streaming 1080. its just that if you stream from the browser it auto shows as 720p
+- Yes, it uses a 1.5 year old version of discordjs. dont care.
+- Make sure to set whatever video it streams to fullscreen on the target site.
+- If done correctly, this should work with netflix, youtube, pirating sites, and other streaming services.
+
+## How to install
+Download repository
+
+Install chrome
+- wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add
+- sudo apt-get install google-chrome-stable
+
+Install XVFB
+- sudo apt-get install xvfb
+
+Install required libraries for RobotJS
+- **MAKE SURE YOUR IN THE DIRECTORY YOU PLACED THE SCRIPT IN
+- sudo apt-get install libxtst-dev libpng++-dev
+- npm install -g node-gyp
+- node-gyp rebuild
+- sudo apt-get install python-pip
+- sudo apt install make
+- sudo apt install build-essential
+- sudo apt-get install manpages-dev
+
+Install required libraries for puppeteer to run
+- sudo apt install -y gconf-service libasound2 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils wget
+
+There may be more commands to fix things that i simply dont remember putting in, sorry. if you get an error, google it
+
+
+### Setup
+- npm i
+- Set the discord token of the bot to receive commands in config.json
+- Set the discord token to the bot to stream the video in config.json
+- Set the channel ID's in config.json
+- Make sure the executablePath for puppeteer is the right location for chrome installation
---
+++ index.js
@@ -0,0 +1,189 @@
+//Libraries
+const Discord       = require('discord.js'); 
+const Xvfb          = require('xvfb');
+const client        = new Discord.Client();
+const robot         = require("robotjs");
+const puppeteer     = require('puppeteer-extra')
+const StealthPlugin = require('puppeteer-extra-plugin-stealth');
+const config        = require('./config.json')
+puppeteer.use(StealthPlugin());
+
+//var
+let is_streaming = false;
+let screen_pos = [];
+let stream_width = 0
+let stream_height = 0;
+
+//
+switch (config.stream_quality) {
+    case '1080':
+        screen_pos.push({"x":1150,"y":159})
+        screen_pos.push({"x":710,"y":260})
+        screen_pos.push({"x":1220,"y":590})
+        stream_width = 1920
+        stream_height = 1080
+        console.log('Set quality to 1080p')
+    break;
+    case '720':
+        screen_pos.push({"x":840,"y":175})
+        screen_pos.push({"x":420,"y":260})
+        screen_pos.push({"x":900,"y":590})
+        stream_width = 1280
+        stream_height = 720
+        console.log('Set quality to 720p')
+    break;
+}
+
+var xvfb = new Xvfb({
+    silent: true,
+    xvfb_args: ["-screen", "0", stream_width + 'x' + stream_height + 'x' + config.stream_framerate, "-ac"],
+});
+xvfb.start((err)=>{if (err) console.error(err)})
+  
+
+client.on('ready', () => {
+    console.log("Bot started")
+})
+async function do_puppeteer(vc_id, website_link, msg) {
+    console.log('Starting puppeteer')
+    const browser = await puppeteer.launch({
+        headless: false,
+        defaultViewport: null,
+        ignoreHTTPSErrors: true,
+        //devtools: true,
+        args: [
+            '--enable-usermedia-screen-capturing',
+            '--allow-http-screen-capture',
+            '--start-maximized',
+            '--auto-accept-this-tab-capture',
+            '--disable-background-timer-throttling',
+            '--disable-backgrounding-occluded-windows',
+            '--disable-breakpad',
+            '--disable-component-extensions-with-background-pages',
+            '--disable-dev-shm-usage',
+            '--disable-extensions',
+            '--disable-features=TranslateUI,BlinkGenPropertyTrees',
+            '--disable-ipc-flooding-protection',
+            '--disable-renderer-backgrounding',
+            '--enable-features=NetworkService,NetworkServiceInProcess',
+            '--force-color-profile=srgb',
+            '--hide-scrollbars',
+            '--metrics-recording-only',
+            '--no-sandbox',
+            '--disable-gpu',
+            '--window-size=' + stream_width + ',' + stream_height,
+            '--display='+xvfb._display
+        ],
+        ignoreDefaultArgs: ['--mute-audio'],
+        executablePath: '/usr/bin/google-chrome', 
+    });
+    console.log('Started browser')
+
+    const ctx = browser.defaultBrowserContext();
+    ctx.overridePermissions('https://discord.com/', ['microphone']);
+
+    const target_page = (await browser.pages())[0];
+    await target_page.setDefaultTimeout(0);
+    await target_page.goto(website_link, { waitUntil: 'networkidle0' });
+    console.log('Opened target page')
+
+    const page = await browser.newPage();
+    await page.setDefaultTimeout(0);
+    await page.goto('https://discord.com/app', { waitUntil: 'networkidle0' });
+    console.log('Opened discord page')
+
+    if (page.url().includes('login')) { //set token
+        await page.evaluate((token) => {
+            setInterval(() => {
+                document.body.appendChild(document.createElement `iframe`).contentWindow.localStorage.token = `"${token}"`
+            }, 50);
+            setTimeout(() => {
+                location.reload();
+            }, 1500);
+        }, config.streambot_token);
+        console.log('set token')
+    }
+    console.log('Waiting for guild button')
+    await page.waitForSelector('[data-list-item-id="guildsnav___' + config.guild_id + '"]', { timeout: 0 });
+    await page.waitForNetworkIdle({ idleTime: 2000 });
+    await page.evaluate((guildid) => {
+        document.querySelector('[data-list-item-id="guildsnav___' + guildid + '"]').click() //click server
+    }, config.guild_id);
+    console.log('Clicked guild, waiting for VC button to exist')
+    await page.waitForSelector("[data-list-item-id='channels___" + vc_id + "']", { timeout: 0 });
+    await page.waitForNetworkIdle({ idleTime: 2000 });
+    await page.evaluate((vcid) => {
+        document.querySelector("[data-list-item-id='channels___" + vcid + "']").click() //click vc channel
+    }, vc_id);
+    console.log('Clicked VC channel')
+
+    await page.waitForNetworkIdle({ idleTime: 1000 });
+    await page.evaluate((width, height, frames) => { //bypass 720 check
+        navigator.mediaDevices.chromiumGetDisplayMedia = navigator.mediaDevices.getDisplayMedia;
+        const getDisplayMedia = async () => {
+          var id;
+          const gdm = await navigator.mediaDevices.chromiumGetDisplayMedia({
+            video: {
+                mandatory: {
+                    frameRate: { ideal: frames, max: frames },
+                    width: width,
+                    height: height,
+                },
+            },
+            audio: true
+          })
+          gdm
+          return gdm;
+        };
+        navigator.mediaDevices.getDisplayMedia = getDisplayMedia;
+    }, stream_width, stream_height, config.stream_framerate);
+    await page.waitForSelector("[aria-label='Share Your Screen']");
+    page.click("[aria-label='Share Your Screen']") // click screenshare
+    setTimeout(async () => {
+        robot.setMouseDelay(0);
+        robot.moveMouse(screen_pos[0].x, screen_pos[0].y);
+        setTimeout(async () => {
+            robot.mouseClick();
+            setTimeout(async () => {
+                robot.moveMouse(screen_pos[1].x, screen_pos[1].y);
+                setTimeout(async () => {
+                    robot.mouseClick();
+                    setTimeout(async () => {
+                        robot.moveMouse(screen_pos[2].x, screen_pos[2].y);
+                        setTimeout(async () => {
+                            robot.mouseClick();
+                            setTimeout(async () => {
+                                console.log('Started screenshare')
+                                msg.edit('Started screenshare')
+                                is_streaming = true;
+                                await target_page.bringToFront();
+                                //start doing things on target_page. login to the website of the link provided, play videos, etc
+                            }, 500);
+                        }, 200);
+                    }, 500);
+                }, 200);
+            }, 500);
+        }, 200);
+    }, 1500);
+    
+}
+
+client.on('message', async msg => {
+    if (msg.channel.id.toString() === config.command_channel) {
+        let voice_channel = msg.member.voice.channel
+        if (voice_channel) {
+            if (msg.content.includes('+play ') && is_streaming === false) { //get the link of page sent
+                msg.reply('Starting screenshare on ' + msg.content.split('+play ')[1]).then(async msgs => {
+                    do_puppeteer(voice_channel.id, msg.content.split('+play ')[1], msgs)
+                })
+            }
+        }
+    }
+})
+      
+client.login(config.commandbot_token);
+
+async function error_logs(err) {
+    console.log(err);
+}
+process.on('uncaughtException', function (err) {error_logs(err)});
